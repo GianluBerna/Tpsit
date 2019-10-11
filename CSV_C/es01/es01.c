09394ce70df8b2a1d0e0d473f75369de46e16f95
Gianluca Bernardi <48619457+GianluBerna@users.noreply.github.com>
2019-10-11 08:56:55 +0200

    Add files via upload

--- CSV_C/es01/es01.c
+++ CSV_C/es01/es01.c
@@ -12,7 +12,7 @@ es01
 #include <string.h>
 
 #define DIM 1000
-#define G 50
+#define G 17000
 
 typedef struct videogame{
     int rank;
@@ -37,7 +37,7 @@ void loadTable(Videogame gl[], int n, int *max, int x){
     char riga1[x];
     char* buffer;
 
-    fp=fopen("test.csv", "r");
+    fp=fopen("vgsales.csv", "r");
     fgets(riga1, x, fp);
     printf("%s\n", riga1);
 
@@ -46,15 +46,19 @@ void loadTable(Videogame gl[], int n, int *max, int x){
            buffer = strtok(appg , ",");
             gl[k].rank = atoi(buffer);
             buffer=strtok(NULL, ",");
-            gl[k].name = buffer;
+            gl[k].name = malloc((strlen(buffer)+1)*sizeof(char));
+            strcpy(gl[k].name , buffer);
             buffer=strtok(NULL, ",");
-            gl[k].platform = buffer;
+            gl[k].platform = malloc((strlen(buffer)+1)*sizeof(char));
+            strcpy(gl[k].platform , buffer);
             buffer=strtok(NULL, ",");
             gl[k].year = atoi(buffer);
             buffer=strtok(NULL, ",");
-            gl[k].genre= buffer;
+            gl[k].genre= malloc((strlen(buffer)+1)*sizeof(char));
+            strcpy(gl[k].genre , buffer);
             buffer=strtok(NULL, ",");
-            gl[k].publisher = buffer;
+            gl[k].publisher = malloc((strlen(buffer)+1)*sizeof(char));
+            strcpy(gl[k].publisher , buffer);
             buffer=strtok(NULL, ",");
             gl[k].na_sales = atof(buffer);
             buffer=strtok(NULL, ",");
@@ -73,9 +77,10 @@ void loadTable(Videogame gl[], int n, int *max, int x){
         printf("Il file non esiste");
 
         }
+        *max=k;
 
-    for(k=0;k<x;k++){
-        printf("%d %s %s %d %s %s %f %f %f %f %f", gl[k].rank, gl[k].name, gl[k].platform, gl[k].year, gl[k].genre, gl[k].publisher, gl[k].na_sales, gl[k].eu_sales, gl[k].jp_sales, gl[k].other_sales, gl[k].global_sales);
+    for(k=0;k<*max;k++){
+        printf("%d %s %s %d %s %s %.2f %.2f %.2f %.2f %.2f\n", gl[k].rank, gl[k].name, gl[k].platform, gl[k].year, gl[k].genre, gl[k].publisher, gl[k].na_sales, gl[k].eu_sales, gl[k].jp_sales, gl[k].other_sales, gl[k].global_sales);
     }
     fclose(fp);
 }
