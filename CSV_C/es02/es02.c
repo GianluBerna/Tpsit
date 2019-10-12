2ad49eec83701c96de116d5cb0928631a3580c8d
Gianluca Bernardi <48619457+GianluBerna@users.noreply.github.com>
2019-10-12 12:57:49 +0200

    Add files via upload

---
+++ CSV_C/es02/es02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int maggiore(int v[], int n);
+
+void main(){
+
+
+    int dim;
+    printf("Inserire la dimensione : ");
+    scanf("%d", &dim);
+    int *vett = malloc((dim)*sizeof(int));
+    int i;
+    for(i=0;i<dim;i++){
+        printf("Inserire il numero [%d] : ", i);
+        scanf("%d", &vett[i]);
+    }
+
+    int m = maggiore(vett, dim);
+    printf("Il maggiore e' : %d" , m);
+
+}
+
+int maggiore(int v[], int n){
+    int i;
+    int max=0;
+    for(i=0;i<n;i++){
+        if(v[i]>max){
+            max=v[i];
+        }
+    }
+    return max;
+}
