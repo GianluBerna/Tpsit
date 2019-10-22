10bac9f7904db26086d46553aece8f9f6a33aa7a
Gianluca Bernardi <48619457+GianluBerna@users.noreply.github.com>
2019-10-22 09:43:07 +0200

    Add files via upload

--- CSV_C/es04/es04.c
+++ CSV_C/es04/es04.c
@@ -6,5 +6,29 @@
 int main()
 {
     /* code */
+
+    int dim;
+    printf("Inserire la dimensione : ");
+    scanf("%d", &dim);
+    int *vett = malloc((dim)*sizeof(int));
+    int i;
+    for(i=0;i<dim;i++){
+        printf("Inserire il numero [%d] : ", i);
+        scanf("%d", vett+i);
+    }
+
+    for(i=0;i<dim;i++){
+        printf("%d : ", i);
+        printf("%d\n", *(vett+i));
+    }
+
+    for(i=0;i<dim;i++){
+        if(*vett < *(vett+i)){
+            *vett = *(vett+i);
+        }
+    }
+
+    printf("Il maggiore e' : %d", *vett);
+
     return 0;
 }
